569a880c31c4dbe99df2458c6be9954f4c57aec6
GameJam <40156487+djns99@users.noreply.github.com>
2019-01-27 15:27:03 +1300

    Fixer uppering

--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -5,21 +5,29 @@ using UnityEngine;
 public class UIScript : MonoBehaviour
 {
     public TimeScript timeScript;
+    public PlayerMovement player1;
+    public PlayerMovement player2;
 
     // Start is called before the first frame update
     void Start()
     {
         Time.timeScale = 0f;
+        player1.gameStopped = true;
+        player2.gameStopped = true;
     }
 
     public void OnEndlessClicked()
     {
+        player1.gameStopped = false;
+        player2.gameStopped = false;
         timeScript.totalSeconds = -1.0f;
         Time.timeScale = 1f;
     }
 
     public void OnTimedClicked()
     {
+        player1.gameStopped = false;
+        player2.gameStopped = false;
         timeScript.totalSeconds = 300.0f;
         Time.timeScale = 1f;
     }
